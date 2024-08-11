7d7370036b7e6d73c2dfb75621a17917fafa09b7
Khayelihle <khayadlamini563@gmail.com>
2024-08-11 02:11:48 +0200

    Profile

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -1,11 +1,15 @@
-const UserProfile = (props) => {
-    return (
-      <div>
-        <h2>{props.name}</h2>
-        <p>Age: {props.age}</p>
-        <p>Bio: {props.bio}</p>
-      </div>
-    );
-  };
+import React from 'react';
 
-  export default UserProfile ;
+const UserProfile = (props) => {
+    const { name, age, bio } = props;
+
+    return (
+        <div>
+            <h2>{name}</h2>
+            <p>Age: {age}</p>
+            <p>{bio}</p>
+        </div>
+    );
+};
+
+export default UserProfile;
