04f0dc665fc84a5bc074e7632e403c63a4e45512
Khayelihle <khayadlamini563@gmail.com>
2024-08-11 02:06:35 +0200

    profile on appjsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,7 @@ import WelcomeMessage from './components/WelcomeMessage';
 import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
+import UserProfile from './components/UserProfile';
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
        <MainContent />
         <Footer />
       <WelcomeMessage />
+      <UserProfile name="Khaya" age="19" bio="Loves Coding(Front-end dev:),Music and Dancing"  />
       
     </>
   )
