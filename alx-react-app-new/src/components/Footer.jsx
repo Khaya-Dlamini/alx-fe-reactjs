f482ca5067785fc681a557106bade628b1c1f768
Khayelihle <khayadlamini563@gmail.com>
2024-08-15 17:50:41 +0200

    styling

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+        <footer style={{padding:'50', color:'grey', textAlign:'center'}}>
+        <p>© 2023 City Lovers</p>
+      </footer>
+    
+    );
+}
+
+export default Footer ;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,10 @@
+function MainContent() {
+    return (
+        <main style={{fontSize: 30, textAlign:'center',}}>
+        <p>I love to visit New York, Paris, and Tokyo.</p>
+      </main>
+    
+    );
+}
+
+export default MainContent ;
