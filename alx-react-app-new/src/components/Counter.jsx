a6b55abc0935dd1514ed82b2e640ae825d3c7e4c
Khayelihle <khayadlamini563@gmail.com>
2024-08-15 18:28:19 +0200

    Counter

--- alx-react-app-new/src/components/Counter.jsx
+++ alx-react-app-new/src/components/Counter.jsx
@@ -7,7 +7,7 @@ function Counter (){
         <div>
             <p>Current count :{count} </p>
             <button onClick={() => setCount(count + 1)}>Increment</button>
-            <button onClick={() => setCount(count - 1)}>Decremnt</button>
+            <button onClick={() => setCount(count - 1)}>Decrement</button>
             <button onClick={() => setCount(0)}>Reset</button>
 
         </div>
