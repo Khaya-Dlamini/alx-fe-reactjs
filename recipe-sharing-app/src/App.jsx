a54e7e27a83cf9f92efe50883ba2a7f02fe9c9e1
Khayelihle <khayadlamini563@gmail.com>
2024-08-22 18:57:11 +0200

    store-recipe-app

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import RecipeList from "./components/RecipeList"
+import AddRecipeForm from "./components/AddRecipeForm"
+
+function App() {
+  
+
+  return (
+  <>
+  <RecipeList/>
+  <AddRecipeForm/>
+   </>
+  )
+}
+
+export default App
