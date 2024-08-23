da28ce51fd4071a8d0b380eb9679a48946231dec
Khayelihle <khayadlamini563@gmail.com>
2024-08-23 17:01:02 +0200

    recipestore

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -5,4 +5,19 @@ const useRecipeStore = create(set => ({
   addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
   setRecipes: (recipes) => set({ recipes })
 }));
+
+const deleteRecipe = create(set => ({
+  recipes: [],
+  deleteRecipe: (id) => set((state) => ({
+    recipes: state.recipes.filter(recipe => recipe.id !== id)
+})),
+}));
+const updateRecipe = create(set => ({
+  recipes: [],
+  UpdateRecipe: (updatedrecipe) => set((state) => ({
+    recipes: state.recipes.map(recipe => recipe.id === updatedrecipe.id ? updatedrecipe : recipe )
+})),
+}));
+export {updateRecipe}
+export {deleteRecipe}
 export {useRecipeStore}
