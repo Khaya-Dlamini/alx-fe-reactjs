ebd55ce39627198a37cb84c5d5a84a64f6a25c61
Khayelihle <khayadlamini563@gmail.com>
2024-08-24 01:30:17 +0200

    faves-recomend

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import RecipeList from './components/RecipeList';
 import RecipeDetails from './components/RecipeDetails';
+import FavoritesList from './components/FavoritesList';
+import RecommendationsList from './components/RecommendationsList';
 
 const App = () => {
     return (
         <Router>
-            <Routes>
-                <Route path="/recipes/:recipeId" element={<RecipeDetails />} />
-                <Route path="/" element={<h1>Welcome to Recipe Manager and AddRecipeForm </h1>} />
-            </Routes>
+            <div style={{ display: 'flex' }}>
+                <div style={{ flex: 1 }}>
+                    <Routes>
+                        <Route path="/recipes/:recipeId" element={<RecipeDetails />} />
+                        <Route path="/recipes" element={<RecipeList />} />
+                        <Route path="/" element={<h1>Welcome to Recipe Manager</h1>} />
+                    </Routes>
+                </div>
+                <div style={{ width: '300px', marginLeft: '20px' }}>
+                    <FavoritesList />
+                    <RecommendationsList />
+                </div>
+            </div>
         </Router>
     );
 };
