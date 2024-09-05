65c07457ae0989a2e703551f1215a4ee349cd337
Khayelihle <khayadlamini563@gmail.com>
2024-09-05 16:28:57 +0200

    tailwind integation

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,14 @@
+import './index.css'; 
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-red-100'>
+       Heyy I'm Khaylihle Dlamini, I am a fullstack developer. 
+      </div>
+    </>
+  )
+}
+
+export default App
