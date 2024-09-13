d1a0c3719bca9a6fc21bec9e1bfb531b0a2215cd
Khayelihle <khayadlamini563@gmail.com>
2024-09-13 16:47:35 +0200

    styles

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -9,13 +9,13 @@ const HomePage = () => {
       .then((data) => setRecipes(data))
       .catch((error) => console.error('Error fetching the recipes:', error));
   }, []);
-
+ 
   return (
     <div>
-      <h1>Recipe List</h1>
-      <div>
+      <h1 className='text-blue-500 hover:text-blue-300 sm:p-4 md:p-8'>Recipe List</h1>
+      <div className='rounded-lg shadow-lg'>
         {recipes.map((recipe) => (
-          <div key={recipe.id}>
+          <div className='sm:p-4 md:p-4' key={recipe.id}>
             <h2>{recipe.title}</h2>
             <p>{recipe.summary}</p>
             <img src={recipe.image} alt={recipe.title} />
