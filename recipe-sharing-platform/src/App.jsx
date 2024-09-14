fc70e6487c11ba97a42ab0571f77723191e4d311
Khayelihle <khayadlamini563@gmail.com>
2024-09-14 17:42:59 +0200

    recipedetail

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import HomePage from './components/HomePage'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import RecipeDetail from './components/RecipeDetail';
 
 function App() {
   return (
+      
     <div>
-      <HomePage/>
-    </div>
-  )
+      
+      <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/recipe" element={<RecipeDetail />} />
+      </Routes>
+        </div>
+        
+      
+  );
 }
 
 export default App
--- recipe-sharing-platform/src/components/RecipeDetail.jsx
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
-const HomePage = () => {
+const RecipeDetail = () => {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
@@ -13,16 +13,18 @@ const HomePage = () => {
  
   return (
     <div>
-      <h1 className='text-blue-500 text-3xl grid justify-items-center'>Recipe List</h1>
+      <h1 className='text-blue-500 text-3xl grid justify-items-center'>Recipe Details</h1>
       <ul>
         <li><Link to = "/" >Recipes</Link></li>
-        <li><Link to = "/recipe" >Recipes Details</Link></li>
+        <li><Link to = "/recipe/:id" >Recipes Details</Link></li>
         </ul> 
       <div className='pt-10 flex items-center flex flex-wrap grid grid-cols-3 gap-12 grid justify-items-center sm:grid grid-cols-2 gap-10'>
         {users.map(user => (
           <div className='bg-gray-400 rounded p-12 hover:shadow-xl'key={user.id}>
             <h1>{user.name}</h1>
+            <h2>{user.username} </h2>
             <p>{user.email}</p>
+            <p>{user.address} </p>
           </div>
         ))}
       </div>
@@ -30,4 +32,4 @@ const HomePage = () => {
   );
 };
 
-export default HomePage;
+export default RecipeDetail;
