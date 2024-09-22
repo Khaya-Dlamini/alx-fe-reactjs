b5a010ee628ed5de00b653b51421c1eb0f1ea429
Khayelihle <khayadlamini563@gmail.com>
2024-09-22 12:52:57 +0200

    edits

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -11,13 +11,12 @@ export const fetchUserData = async ({ username, location, minRepos }) => {
       query += `repos:>${minRepos} `;
     }
 
-    const response = await axios.get(`https://api.github.com/search/users?${query}`);
+    const response = await axios.get(`https://api.github.com/search/users?q${query}`);
     return response.data.items; 
   } catch (error) {
     throw new Error('Error fetching users');
   }
 };
-
 
 
 
